65544240d95031d2db33d1e17fc74b6cdddbcc96
ManuelRRo <ds23906@gmail.com>
2025-06-12 12:58:26 -0600

    Add validationPipe Auto Validate Coffee input name lenght 3

--- src/coffees/dto/create-coffee.input.ts
+++ src/coffees/dto/create-coffee.input.ts
@@ -1,6 +1,8 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { MinLength } from 'class-validator';
 @InputType({ description: 'Create a Coffee Input Object Type' })
 export class CreateCoffeeInput {
+  @MinLength(3)
   @Field(() => String, { description: 'A new coffee name' })
   name: string;
   @Field()
