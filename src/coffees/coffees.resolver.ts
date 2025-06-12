35f32547405090eddef40e5ebb319ee98efd7f94
ManuelRRo <ds23906@gmail.com>
2025-06-12 12:53:06 -0600

    Coffee: Adding delete and update mutation

--- src/coffees/coffees.resolver.ts
+++ src/coffees/coffees.resolver.ts
@@ -3,21 +3,36 @@ import { Coffee } from './entities/coffee.entity';
 import { ParseIntPipe } from '@nestjs/common';
 import { CreateCoffeeInput } from './dto/create-coffee.input';
 import { CoffeesService } from './coffees.service';
+import { UpdateCoffeeInput } from './dto/update-coffee.input';
 
 @Resolver()
 export class CoffeesResolver {
-  constructor(private readonly coffeeService: CoffeesService) {}
+  constructor(private readonly coffeesService: CoffeesService) {}
+
   @Query(() => [Coffee], { name: 'coffees' })
   async findAll() {
-    return this.coffeeService.findAll();
+    return this.coffeesService.findAll();
   }
   @Query(() => Coffee, { name: 'coffee', nullable: true })
   findOne(@Args('id', { type: () => ID }, ParseIntPipe) id: number) {
-    return this.coffeeService.findOne(id);
+    return this.coffeesService.findOne(id);
   }
 
   @Mutation(() => Coffee, { name: 'createCoffee', nullable: true })
   create(@Args('createCoffeeInput') createCoffeeInput: CreateCoffeeInput) {
-    return this.coffeeService.create(createCoffeeInput);
+    return this.coffeesService.create(createCoffeeInput);
+  }
+
+  @Mutation(() => Coffee, { name: 'updateCoffee' })
+  async update(
+    @Args('id', ParseIntPipe) id: number,
+    @Args('updateCoffeeInput') updateCoffeeInput: UpdateCoffeeInput,
+  ) {
+    return this.coffeesService.update(id, updateCoffeeInput);
+  }
+
+  @Mutation(() => Coffee, { name: 'removeCoffee' })
+  async remove(@Args('id', ParseIntPipe) id: number) {
+    return this.coffeesService.remove(id);
   }
 }
